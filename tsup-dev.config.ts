e3a991abc2606c4d27453a94b5896618221f842a
Cleboost <clement.balarot@gmail.com>
2025-01-08 14:39:22 +0100

    chore: remove external dependency from config (not needed)

--- tsup-dev.config.ts
+++ tsup-dev.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	format: ["cjs"],
 	entryPoints: ["src/index.ts"],
-	external: ["test"],
 	outDir: "dist",
 	onSuccess: "cd test && tsup",
 	dts: true,
