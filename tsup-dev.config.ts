4180a08b31c42683e37099d35a95ad2af9f9d1fe
Cleboost <clement.balarot@gmail.com>
2025-02-01 12:31:57 +0100

    fix🐛: update success command in tsup config

--- tsup-dev.config.ts
+++ tsup-dev.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   format: ["cjs"],
   entry: ["src/index.ts"],
   outDir: "dist",
-  onSuccess: "cd playground && pnpm run dev",
+  onSuccess: "cd playground && pnpm i && pnpm run start",
   dts: true,
   skipNodeModulesBundle: true,
   clean: true,
