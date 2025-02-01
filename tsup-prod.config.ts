0f92848146ad86cce35abaf434a57ec039b9a63d
Cleboost <clement.balarot@gmail.com>
2025-02-02 00:02:35 +0100

    📇 refactor: remove redundant file copying logic in build configuration

--- tsup-prod.config.ts
+++ tsup-prod.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "tsup";
-import fs from "fs";
 
 export default defineConfig({
   format: ["cjs"],
@@ -8,14 +7,5 @@ export default defineConfig({
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
-  minify: true,
-  onSuccess: async () => {
-    fs.copyFileSync("package.json", "dist/package.json");
-    fs.copyFileSync("README.md", "dist/README.md");
-    const packageString = fs.readFileSync("dist/package.json", "utf-8");
-    const packageJson = JSON.parse(packageString);
-    delete packageJson.devDependencies;
-    delete packageJson.scripts;
-    fs.writeFileSync("dist/package.json", JSON.stringify(packageJson, null, 2));
-  },
+  minify: true
 });
