16ac9fb1da60364144e889b1142a1abf0ec8b73b
Cleboost <clement.balarot@gmail.com>
2025-02-24 13:27:35 +0100

    feat✨: update GitHub workflows for pnpm setup and release process

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.[jt]s?(x)"],
-  // moduleNameMapper: {
-  //   "^src/(.*)$": "<rootDir>/src/$1"
-  // }
 };
