08d20cf6d3fef965e9fbca715e204038b332b4d6
Cleboost <clement.balarot@gmail.com>
2025-07-11 10:19:38 +0200

    Add SuperDemoPlugin example and integrate with bot config
    Introduces a new example plugin, SuperDemoPlugin, in the examples/plugins directory with type extension for discord.js Client. Updates the bot configuration to load this plugin, demonstrating plugin integration and extension of client properties.

--- examples/bot/djsconfig.ts
+++ examples/bot/djsconfig.ts
@@ -1,4 +1,5 @@
 import { GatewayIntentBits } from "discord.js";
+import { SuperDemoPlugin } from "my-super-djs-plugin";
  
 export default {
   token: process.env.TOKEN ?? "", // To be overridden in .env
@@ -6,4 +7,5 @@ export default {
   // Add your test server ID here for immediate deployment,
   // or leave empty for global deployment (which takes longer).
   guildIds: ["861553974728327168"],
+  plugins: [SuperDemoPlugin()]
 } as const; 
