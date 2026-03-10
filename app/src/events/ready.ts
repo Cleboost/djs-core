f8dc6b351b048d8cb8b5bf3ca4006ec304bd58f7
Cleboost <clement.balarot@gmail.com>
2026-03-10 11:29:01 +0100

    refactor: improve client type in EventListener

--- app/src/events/ready.ts
+++ app/src/events/ready.ts
@@ -2,5 +2,5 @@ import { EventListener } from "@djs-core/runtime";
 import { ActivityType, Events } from "discord.js";
 
 export default new EventListener().event(Events.ClientReady).run((client) => {
-	client.user?.setActivity({ name: "🥖 bread", type: ActivityType.Custom });
+	client.user.setActivity({ name: "🥖 bread", type: ActivityType.Custom });
 });
