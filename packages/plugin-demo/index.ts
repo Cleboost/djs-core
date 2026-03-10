5500a11c2db8052452288e1eba8d785b7ebadfd9
Cleboost <clement.balarot@gmail.com>
2026-03-10 13:32:57 +0100

    feat(plugins): add demo plugin package for testing

---
+++ packages/plugin-demo/index.ts
@@ -0,0 +1,21 @@
+import { definePlugin } from "@djs-core/runtime";
+
+export interface DemoConfig {
+	message: string;
+}
+
+export const demoPlugin = definePlugin({
+	name: "demo",
+	setup: (client, config: DemoConfig) => {
+		return {
+			sayHello: () => {
+				console.log(`[Demo Plugin] ${config.message}`);
+				return `Hello: ${config.message}`;
+			},
+		};
+	},
+	onReady: (client, config, extension) => {
+		console.log("[Demo Plugin] Ready!");
+		extension.sayHello();
+  },
+});
